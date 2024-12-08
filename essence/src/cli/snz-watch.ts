527a64f2cfe043dde057a7d2d98f55b7645bd502
Pete Hodgson <git@thepete.net>
2024-12-07 21:49:11 -0800

    fix module resolution for jsx/tsx

--- essence/src/cli/snz-watch.ts
+++ essence/src/cli/snz-watch.ts
@@ -12,6 +12,15 @@ const targetDir = process.argv[2] || "."
 // If it's not, then we assumed we're restarting because of a file change.
 process.env["NODEMON_ESSENCE_TIMESTAMP"] = new Date().getTime().toString()
 
+// Hacky way to ensure that our dependencies (specifically `nano-jsx`) are resolvable when we
+// dynamically import source files from the target dir. This is necessary for .jsx/.tsx source files,
+// which will implicitly import `nano-jsx`, because we set `jsxImportSource` to `nano-jsx/lib`.
+//
+// This hack is needed when someone runs `npx snz`, because in that scenario the `snz` modules dependencies
+// are not in a resolvable location.
+
+const this_modules_node_modules_dir = path.resolve(__dirname, "..", "..", "node_modules")
+
 nodemon({
   script: path.join(__dirname, "snz.js"),
   // @ts-ignore: TS2353 - args is a working parameter accepted by `nodemon()`, but not documented in the type definition
@@ -19,4 +28,7 @@ nodemon({
   watch: [targetDir],
   verbose: true,
   ext: "*",
+  env: {
+    NODE_PATH: this_modules_node_modules_dir,
+  },
 })
